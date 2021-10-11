e29135af124d3289d8c22f6c41e1e54ab71c8855
Ishan Rai <ishan.rai1907@gmail.com>
2021-10-11 15:12:55 +0530

    added a note for division.java

---
+++ division.java
@@ -0,0 +1,9 @@
+public class division {
+    public static void main(String[] args) {
+        int a = 9;
+        int b = 8;
+        System.out.println("Quotient is = " + (a / b));
+        System.out.println("thank you");
+
+    }
+}
